7f3137bb254d52989ddcaf9b410a42f5a2f4260f
Varun <sharmavarun.1912@gmail.com>
2024-04-11 09:33:36 +0530

    Fixed footer layout on mobile

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,13 +7,13 @@ import { TbBrandNextjs } from "react-icons/tb";
 
 const Footer = () => {
   return (
-    <footer className="w-screen p-4 mt-16 flex flex-col justify-center items-center">
+    <footer className="w-screen py-4 sm:px-4 mt-16 flex flex-col justify-center items-center">
       <div className="w-full h-0.5 bg-gradient-to-r from-[#c6d4ff] to-[#a8ffe3] mb-4"></div>
-      <div className="w-full flex flex-wrap justify-between items-center gap-4 px-16">
+      <div className="w-full flex flex-col sm:flex-row justify-between items-center gap-4 sm:px-16">
         <small className="font-semibold">
           &copy; Varun Sharma. All Rights Reserved.
         </small>
-        <div className="flex gap-x-4">
+        <div className="flex gap-x-4 justify-center items-center">
           <FaReact className="text-2xl text-blue-500" />
           <SiFramer className="text-2xl text-black" />
           <TbBrandNextjs className="text-2xl text-black" />
